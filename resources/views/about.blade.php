9f1beff303e76d5c0e15d2aca322b389504ae2ad
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-13 10:10:04 +0000

    Le teacher du net

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,13 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello-city</title>       
+        <title>About Us | Hello-city</title>       
       
     </head>
     <body>
-            <h1>Hello From Quebec !</h1>
-
-            <p>It's currenty {{ date('h:i A')}}.</p>
+            <p>Build white &hearts; By TEACHER DU NET.</p>
+            <P><a href="/">Revenir à la page d'acceuil</a></P>
 
             <footer>
                 <p>&copy; Copiryght {{ date('Y')}} &middot; <a href="/about-us">About us</a></p>
