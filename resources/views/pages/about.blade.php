3f6481833ea6b075b4ab6ca8312fa36addbcef80
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-13 11:02:27 +0000

    Add proper title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,11 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About Us | Hello-city</title>       
-      
-    </head>
-    <body>
+@section('title', 'About Us | Hello-city')
+
+
+@section('content')
+
             <p>Build white &hearts; By TEACHER DU NET.</p>
             <P><a href="/">Revenir à la page d'acceuil</a></P>
 
-            <footer>
-                <p>&copy; Copiryght {{ date('Y')}} &middot; </p>
-            </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Hello-city</title>       
-      
-    </head>
-    <body>
+@section('title', 'Hello-city')
+
+    
+
+@section('content')
             <h1>Hello From Quebec !</h1>
 
-            <p>It's currenty {{ date('h:i A')}}.</p>
+            <p>It's currenty {{ date('h:i A')}}.</p>     
 
-            <footer>
-                <p>&copy; Copiryght {{ date('Y')}} &middot; <a href="/about-us">About us</a></p>
-            </footer>
-    </body>
-</html>
+@endsection
