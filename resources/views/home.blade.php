99e17f7bc00b1678b4a822dab74fc727fb85531a
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-13 09:48:38 +0000

    teacher du net

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello-city</title>       
+      
+    </head>
+    <body>
+            <h1>Hello From Quebec !</h1>
+
+            <p>It's currenty {{ date('h:i A')}}.</p>
+
+            <footer>
+                <p>&copy; Copiryght {{ date('Y')}} &middot; <a href="#">About us</a></p>
+            </footer>
+    </body>
+</html>
